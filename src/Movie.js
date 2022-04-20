28b309f3d8ba6dbec044cba5dff886fc23ba1f2a
qwerewqwerew <qwerew0@gmail.com>
2022-04-20 11:08:21 +0900

    07 깃허브에 리액트 앱 업로드 하기

--- src/Movie.js
+++ src/Movie.js
@@ -2,15 +2,24 @@ import React from "react"
 import PropTypes from "prop-types"
 import "./Movie.css"
 
-function Movie({  title, year, summary, poster }) {
+function Movie({ title, year, summary, poster, genres }) {
 	return (
-        <div className="movie__data">
-            <img src={poster} alt={title} title={title} />
-            <h3 className="movie__title">{title}</h3>
-            <h5 className="movie__year">{year}</h5>
-            <p className="movie__summary">{summary}</p>
-        </div>
-    )
+		<div className="movie__data">
+			<img src={poster} alt={title} title={title} />
+			<h3 className="movie__title">{title}</h3>
+			<h5 className="movie__year">{year}</h5>
+			<ul className="movie__genres">
+				{genres.map((genre, index) => {
+					return (
+						<li key={index} className="movie_genre">
+							{genre}
+						</li>
+					)
+				})}
+			</ul>
+			<p className="movie__summary">{summary.slice(0,180)}...</p>
+		</div>
+	)
 }
 //title,summery,medium_cover_image->poster
 Movie.propTypes = {
@@ -18,6 +27,7 @@ Movie.propTypes = {
 	title: PropTypes.string.isRequired,
 	summary: PropTypes.string.isRequired,
 	poster: PropTypes.string.isRequired,
+	genres: PropTypes.arrayOf(PropTypes.string).isRequired,
 }
 
 export default Movie
